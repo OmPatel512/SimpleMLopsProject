5064c706e67b6f622574b07110df57383c4d7b69
Om Patel <omsuperson@gmail.com>
2025-07-26 18:01:01 +0530

    template.py updated

--- template.py
+++ template.py
@@ -2,6 +2,9 @@ import os
 from pathlib import Path
 import logging
 
+logging.basicConfig(
+    level=logging.INFO,)
+
 list_of_files = [
     ".github/workflows/.gitkeep",
     "src/__init__.py",
@@ -32,11 +35,10 @@ for filepath in list_of_files:
         os.makedirs(filedir, exist_ok=True)
         logging.info(f"Creating directory {filedir} for the file: {filename}")
         
-    if (not os.path.exists(filepath)) or (os.path.getsize(filepath)==0):
-        with open(filepath,"w") as f:
-            
+    if (not filepath.exists()) or (filepath.stat().st_size == 0):
+        with open(filepath,"w") as f:  
             pass
-            logging.info(f"Creating empty file: {filepath}")
+        logging.info(f"Creating empty file: {filepath}")
     
     else:
         logging.info(f"{filename} is already exists")
