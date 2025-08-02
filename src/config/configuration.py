7a54f251cf74c6345e99f505d63cbdf89123520b
Om Patel <omsuperson@gmail.com>
2025-08-02 12:30:47 +0530

    Project Completed

--- src/config/configuration.py
+++ src/config/configuration.py
@@ -6,10 +6,10 @@ from src.entity.config_entity import DataIngestionConfig, DataValidationConfig, 
 class ConfigurationManager:
     def __init__(self, config_filepath=CONFIG_FILE_PATH,
                  params_filepath=PARAMS_FILE_PATH,
-                 scherma_filepath=SCHEMA_FILE_PATH):
+                 schema_filepath=SCHEMA_FILE_PATH):
         self.config = read_yaml(config_filepath)
         self.params = read_yaml(params_filepath)
-        self.schema = read_yaml(scherma_filepath)
+        self.schema = read_yaml(schema_filepath)
         create_directories([self.config.artifacts_root])
         
     def get_data_ingestion_config(self) -> DataIngestionConfig:
